e639a581d2ff1deb298830ce46d1789e86d3fa7e
Neven Bedr <neven2225@gmail.com>
2020-05-15 01:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
+
 document.addEventListener("keydown", function(e) {
   console.log(e.key);
 });
