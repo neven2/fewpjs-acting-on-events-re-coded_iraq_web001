10dc35846c567758f257276940aa4f6610c663dc
Neven Bedr <neven2225@gmail.com>
2020-05-15 01:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
